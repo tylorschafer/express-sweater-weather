654d12bea1381f774dbf80aafbb3fd3f52aa9a90
Tylor Schafer <tylorschafer@gmail.com>
2019-11-19 20:22:03 -0700

    Add userId check in post favorites endpoint

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -7,20 +7,22 @@ const database = require('knex')(configuration)
 
 router.post('/', (request, response) => {
   const favorite = request.body
+  const userId = database.select('id').from('users').where('api_key', request.body.api_key)
 
   for (const requiredParameter of ['location', 'api_key']) {
-    if (!favorite[requiredParameter]) {
+    if (!favorite[requiredParameter] || !userId) {
       return response
         .status(422)
         .send({ error: `Expected format: { location: <STRING>, api_key: <STRING}. You're missing a "${requiredParameter}" property.` })
     }
   }
-  database('favorites').insert(favorite, 'id')
+
+  database('favorites').insert(favorite, 'id', userId)
     .then(favorite => {
       response.status(200).json({ message: `"${favorite.location}" has been added to your favorites` })
     })
     .catch(error => {
-      response.status(500).json({ error });
+      response.status(500).json({ error })
     })
 })
 

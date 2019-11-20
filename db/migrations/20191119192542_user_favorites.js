08848ca8eaa62a8d958cf044a914c742a0dd278b
Tylor Schafer <tylorschafer@gmail.com>
2019-11-19 19:32:12 -0700

    Refactor users and favorites into single migration to prevent foreign key constraint

--- db/migrations/20191119192542_user_favorites.js
+++ db/migrations/20191119192542_user_favorites.js
@@ -1,10 +1,19 @@
 
 exports.up = function (knex) {
   return Promise.all([
+    knex.schema.createTable('users', function (table) {
+      table.increments('id').primary()
+      table.string('api_key')
+
+      table.timestamps(true, true)
+    }),
+
     knex.schema.createTable('favorites', function (table) {
       table.increments('id').primary()
       table.string('location')
-      table.foreign('user_id').references('users.id')
+      table.integer('user_id').unsigned()
+      table.foreign('user_id')
+        .references('users.id')
 
       table.timestamps(true, true)
     })
@@ -13,6 +22,7 @@ exports.up = function (knex) {
 
 exports.down = function (knex) {
   return Promise.all([
+    knex.schema.dropTable('users'),
     knex.schema.dropTable('favorites')
   ])
 }

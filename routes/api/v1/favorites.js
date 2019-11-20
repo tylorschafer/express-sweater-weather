2759e308e3ca6bc31577ea0572ca75c2feaada16
Tylor Schafer <tylorschafer@gmail.com>
2019-11-19 20:05:12 -0700

    Add favorites router post favorite endpoint

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -1,21 +1,27 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../../../knexfile')[environment];
-const database = require('knex')(configuration);
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('../../../knexfile')[environment]
+const database = require('knex')(configuration)
 
-app.post('api/v1/favorites', (request, response) => {
-  const favorite = request.body 
+router.post('api/v1/favorites', (request, response) => {
+  const favorite = request.body
 
-  for (let requiredParameter of ['location', 'api_key']) {
+  for (const requiredParameter of ['location', 'api_key']) {
     if (!favorite[requiredParameter]) {
       return response
         .status(422)
         .send({ error: `Expected format: { location: <STRING>, api_key: <STRING}. You're missing a "${requiredParameter}" property.` })
     }
   }
-
+  database('favorites').insert(favorite, 'id')
+    .then(favorite => {
+      response.status(200).json({ message: `"${favorite.location}" has been added to your favorites` })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    })
 })
 
-module.exports = router;
+module.exports = router

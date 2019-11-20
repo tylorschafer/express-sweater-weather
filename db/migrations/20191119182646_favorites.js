18fb0bfa0fac5e273cf1b1f580de3fea9b685a59
Tylor Schafer <tylorschafer@gmail.com>
2019-11-19 18:32:22 -0700

    Add favorites table migration

--- db/migrations/20191119182646_favorites.js
+++ db/migrations/20191119182646_favorites.js
@@ -1,9 +1,10 @@
 
 exports.up = function (knex) {
   return Promise.all([
-    knex.schema.createTable('users', function (table) {
+    knex.schema.createTable('favorites', function (table) {
       table.increments('id').primary()
-      table.string('api_key')
+      table.string('location')
+      table.foreign('user_id').references('users.id')
 
       table.timestamps(true, true)
     })
@@ -12,6 +13,6 @@ exports.up = function (knex) {
 
 exports.down = function (knex) {
   return Promise.all([
-    knex.schema.dropTable('users')
+    knex.schema.dropTable('favorites')
   ])
 }

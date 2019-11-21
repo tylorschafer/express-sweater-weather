3f1af4a197bfcac791bce915f79eff7518580dfd
Tylor Schafer <tylorschafer@gmail.com>
2019-11-20 18:10:44 -0700

    Add google geocode fetch call attempt

--- routes/api/v1/forecasts.js
+++ routes/api/v1/forecasts.js
@@ -6,11 +6,28 @@ const configuration = require('../../../knexfile')[environment]
 const database = require('knex')(configuration)
 const fetch = require('node-fetch')
 
-router.get('/', (request, response) => {
-  fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_API_KEY}&address=denver, co`)
-    .then(response => response.json())
-    .then(result => console.log(result))
+function findByKey (key) {
+  return database.select('id').from('users').where('api_key', key).first()
+}
+
+function googleGeocode (address) {
+  return fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_API_KEY}&address=${address}`)
     .catch((error) => console.error({ error }))
+}
+
+router.get('/', (request, response) => {
+  (async () => {
+    const key = request.body.api_key
+    const userId = await findByKey(key).then(result => { return result })
+    const address = request.query.location
+
+    if (userId) {
+      var coordinates = await googleGeocode(address).then(result => JSON.stringify(result.body))
+      return console.log(coordinates)
+    } else {
+      response.status(422).send({ error: 'Bad api_key' })
+    }
+  })()
 })
 
 module.exports = router
